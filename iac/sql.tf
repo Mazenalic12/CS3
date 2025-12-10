c9a011740efb0bebd34327885a7cff0705ff9a4f
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2025-12-10 10:51:39 +0000

    Initial project files + automation

--- iac/sql.tf
+++ iac/sql.tf
@@ -3,42 +3,43 @@ resource "google_compute_global_address" "sql_private_ip" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  # Gebruik de self_link i.p.v. id
-  network = google_compute_network.vpc.self_link
+  network       = google_compute_network.vpc.id
 }
 
 resource "google_service_networking_connection" "sql_vpc_connection" {
-  # Ook hier self_link gebruiken
-  network                 = google_compute_network.vpc.self_link
+  network                 = google_compute_network.vpc.id
   service                 = "services/servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.sql_private_ip.name]
 }
 
 resource "google_sql_database_instance" "hr" {
-  name             = var.db_instance_name # bijv. "innovatech-hr-sql"
+  name             = var.db_instance_name
   database_version = "POSTGRES_15"
-  region           = var.region # bijv. "europe-west1"
+  region           = var.region
 
-  # WACHT eerst op service networking peering
   depends_on = [google_service_networking_connection.sql_vpc_connection]
 
   settings {
-    # LET OP: var.db_tier moet een geldige Cloud SQL tier zijn, zoals "db-f1-micro"
     tier = var.db_tier
 
     ip_configuration {
-      ipv4_enabled = false
-      # Ook hier de self_link van de VPC
-      private_network = google_compute_network.vpc.self_link
+      # **PRIVATE IP** voor GKE / Zero Trust
+      private_network = google_compute_network.vpc.id
+
+      # **PUBLIC IP** automatisch aanzetten voor jouw testen
+      ipv4_enabled = true
+
+      # Zelfde als wat je nu met de hand in de console hebt:
+      authorized_networks {
+        name  = "anywhere-dev"
+        value = "0.0.0.0/0"
+      }
     }
 
     backup_configuration {
       enabled = true
     }
   }
-
-  # Handig tijdens ontwikkelen, anders kan hij soms niet weg
-  deletion_protection = false
 }
 
 resource "google_sql_user" "hr_user" {
